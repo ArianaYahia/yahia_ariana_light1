0956811053bab1f6fe16789a63bf4de5442d5c12
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2020-12-17 18:31:57 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+while True:
+    print("Light Level:" + input.light_level())
+    if input.light_level() < 6: 
+        light.set_all(light.rgb(0, 0, 255 ))
+    elif input.light_level() > 13:
+        light.set_all(light.rgb(0, 0, 0 ))
+    else:
+        light.set_all(light.rgb(245, 149, 59 ))
